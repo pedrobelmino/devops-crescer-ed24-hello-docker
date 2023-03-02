e38c83d1961197d56c5f7d1d9990b2952dfbe340
Pedro Belmino <pedrobelmino@gmail.com>
2023-03-02 10:35:23 -0300

    Revert "Compose"

--- src/main/java/br/com/cwi/hellodocker/HellodockerApplication.java
+++ src/main/java/br/com/cwi/hellodocker/HellodockerApplication.java
@@ -2,9 +2,8 @@ package br.com.cwi.hellodocker;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
 
-@SpringBootApplication(exclude = {MongoAutoConfiguration.class})
+@SpringBootApplication
 public class HellodockerApplication {
 
 	public static void main(String[] args) {
